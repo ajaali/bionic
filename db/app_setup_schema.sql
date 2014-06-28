9dab241af11e170aec429af3b84ccc94407d7046
Ahmed Ali <aja.ali@gmail.com>
2014-06-28 23:49:51 +0100

    Correcting User-Group-Permissions relation

--- db/app_setup_schema.sql
+++ db/app_setup_schema.sql
@@ -9,39 +9,40 @@ CREATE TABLE app (
   app_name              VARCHAR(60) UNIQUE NOT NULL,
   app_key               UUID UNIQUE NOT NULL,
   private_key           UUID UNIQUE NOT NULL,
+  settings              JSONB,
   PRIMARY KEY(app_key)
-);
-
-CREATE TABLE app_setings (
-  app_key               UUID REFERENCES app (app_key),
-  settings              JSONB
 );
 
 CREATE TABLE app_user (
   user_id               SERIAL,
-  app_key               UUID REFERENCES app (app_key),
   username              VARCHAR(60) UNIQUE NOT NULL,
   email                 VARCHAR(250) UNIQUE NOT NULL,
   "passowrd"            CHAR(256),
+  details               JSONB,
   PRIMARY KEY(user_id)
-
-);
-
-CREATE TABLE user_details (
-  user_id               INTEGER REFERENCES app_user (user_id),
-  details               JSONB
 );
 
 CREATE TABLE app_group (
   group_id              SERIAL,
   app_key               UUID REFERENCES app (app_key),
   group_name            VARCHAR(60) UNIQUE NOT NULL,
-  read_permission       BOOLEAN,
-  write_permission      BOOLEAN,
   PRIMARY KEY(group_id)
 );
 
 CREATE TABLE group_user (
   group_id              INTEGER REFERENCES app_group (group_id),
   user_id               INTEGER REFERENCES app_user (user_id)
+);
+
+CREATE TABLE permission (
+  permission_id         SERIAL,
+  permission_name       VARCHAR(30),
+  allow_read            BOOLEAN DEFAULT FALSE,
+  allow_write           BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY(permission_id)
+);
+
+CREATE TABLE group_permission (
+  group_id              INTEGER REFERENCES app_group (group_id),
+  permission_id         INTEGER REFERENCES permission (permission_id)
 );
